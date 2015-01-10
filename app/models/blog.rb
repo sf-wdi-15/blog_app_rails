57fa437280e3939d1661d501d4b81ce8fb0807a4
sandra stark <stark.sandy@gmail.com>
2015-01-09 16:48:11 -0800

    models,blog.rb

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,12 +1,20 @@
-class Blog < ActiveRecord::Base
-  def make_request
-    res = Typhoeus.get("http://www.alchemyapi.com/",{
-    params:{
-      q: @title
-    }
-    })
-    res.body
-  end
+class Article < ActiveRecord::Base
+def make_request
+request = Typhoeus::Request.new(
+  "http://access.alchemyapi.com/calls/text/TextGetRankedKeywords",
+  method: :get,
+  params: { text: text, apikey: "3332801afd31fe72ab705bc43d0bf699cc431c7b", keywordExtractMode: "normal" },
+  headers: { Accept: "text/html" }
+)
+
+request.run
+
+response = request.response
+response.code
+response.total_time
+response.headers
+response.body
+end  
 end
 
 
