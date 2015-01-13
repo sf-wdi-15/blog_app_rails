25291d961e28b6a067a0fb847dda52a8e67ee2e8
Alex Fulsome <alexfulsome@gmail.com>
2015-01-13 11:28:22 -0800

    fixed api

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,19 +1,19 @@
 class Article < ActiveRecord::Base
-	def make_request
-		request = Typhoeus::Request.new(
-  		"http://access.alchemyapi.com/calls/text/TextGetRankedKeywords",
-  		method: :get,
-  		params: { text: text, apikey: "076b4786e580dc41c326bab6535d6bc92371a94d", keywordExtractMode: "normal" },
-  		headers: { Accept: "text/html" }
-		)
+  attr_accessor :keywords
+  @@alchemy_url ||= ENV["ALCHEMY_URL"]
 
-		request.run
 
-		response = request.response
-		response.code
-		response.total_time
-		response.headers
-		response.body
-		
-	end   
+  def make_request
+    res = Typhoeus.get(@@alchemy_url, params: {
+      apikey: ENV["076b4786e580dc41c326bab6535d6bc92371a94"],
+      text: content,
+      maxRetrieve: 10,
+      outputMode: "json"
+    })
+    words = JSON.parse(res.body)["keywords"].map do |w|
+      w['text']
+    end
+    @keywords = words.join(" | ")
+  end
+
 end
