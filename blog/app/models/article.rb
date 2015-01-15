895bdcbc5577d7bd49242bb3d393f3df834bda91
Alex Fulsome <alexfulsome@gmail.com>
2015-01-15 00:43:20 -0800

    views

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,19 +1,30 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+
   attr_accessor :keywords
-  @@alchemy_url ||= ENV["ALCHEMY_URL"]
 
+  @@alchemy_url ||= ENV["076b4786e580dc41c326bab6535d6bc92371a94"]
 
-  def make_request
-    res = Typhoeus.get(@@alchemy_url, params: {
-      apikey: ENV["076b4786e580dc41c326bab6535d6bc92371a94"],
-      text: content,
-      maxRetrieve: 10,
-      outputMode: "json"
-    })
-    words = JSON.parse(res.body)["keywords"].map do |w|
-      w['text']
-    end
-    @keywords = words.join(" | ")
+  def author
+    user ? user.email : "N/A"
+  end
+
+  def owned_by?(user)
+    self.user_id == user.id
+  end
+
+  def get_keywords
+    # res = Typhoeus.get(@@alchemy_url, params: {
+    #   apikey: ENV["076b4786e580dc41c326bab6535d6bc92371a94"],
+    #   text: content,
+    #   maxRetrieve: 10,
+    #   outputMode: "json"
+    # })
+    # words = JSON.parse(res.body)["keywords"].map do |w|
+    #   w['text']
+    # end
+    # @keywords = words.join(" | ")
+    @keywords = "not yet implemented"
   end
 
 end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109020542) do
+ActiveRecord::Schema.define(version: 20150115013159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.string   "author"
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
